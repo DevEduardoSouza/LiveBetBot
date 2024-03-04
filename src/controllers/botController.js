8a92943e172cb528e3ed59430d7b6e17f0835e61
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-03-04 13:27:11 -0300

    feat: reset a lógica de scraper

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -1,8 +1,7 @@
 import { initializeBrowser } from "../services/browserService.js";
 import { configs } from "../config/configs.js";
-import * as cheerio from "cheerio";
 
-const URL = "https://tipmanager.net/" || configs.bet365.url;
+const URL = configs.googleMaps.url;
 
 export async function scrapeData() {
   try {
@@ -10,38 +9,6 @@ export async function scrapeData() {
     await page.goto(URL);
     await page.waitForLoadState("networkidle");
     await page.waitForTimeout(2000);
-
-    /**
-     * Scraping das partidas AOVIVO do https://tipmanager.net/
-     */
-    const dataElement = await page.locator("#__NEXT_DATA__").allInnerTexts();
-    const jsonData = JSON.parse(dataElement);
-
-    // Pegar apenas os e-Soccer
-
-    // Pegar as todas as próximas partidas
-    const nextMatches = jsonData.props.pageProps.nextMatches;
-
-    // Pegar so E-soccer
-    const listEsoccer = nextMatches.filter(
-      (match) => match.sport === "e-Soccer"
-    );
-    console.log(listEsoccer);
-
-    /**
-     * Pegar so dados dos jogos aovivos
-     */
-    await page.goto(configs.bet365.url);
-    await page.waitForLoadState("networkidle");
-    await page.waitForTimeout(2000);
-
-    // Clicar em e-soccer
-    //Clicar em aceitar cookies
-    // await page.locator("div.ccm-CookieConsentPopup_Accept").click();
-    // await page.pause();
-    await page.locator(".ovm-DisciplineSwitcher_Button > div").click();
-    await page.getByText("E-soccer").first().click();
-    await page.screenshot({ path: "printBEt.png" });
 
     // await page.close();
   } catch (error) {
