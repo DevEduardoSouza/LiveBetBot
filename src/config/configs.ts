4e569f907253ae9b88c45778caed7745927679b1
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-05-20 11:24:44 -0300

    feat: add setup inital

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,1 +1,10 @@
-export const configs = {};
+export const configs = {
+  siteName: process.env.SITE_NAME,
+  siteUrl: process.env.SITE_URL,
+  siteKeywords: process.env.SITE_KEYWORDS || "test",
+  siteLanguage: process.env.SITE_LANGUAGE || "pt-BR",
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3",
+  },
+};
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { scrapeData } from "./controllers/botController";
-
-async function init() {
-  await scrapeData();
-}
-
-init();
